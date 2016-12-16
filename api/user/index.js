8caab5b16778ab44b1f65f5b00761e9079ed896d
William Le <williamle8300@gmail.com>
2016-12-16 14:44:03 -0800

    asdf

--- api/user/index.js
+++ api/user/index.js
@@ -1,19 +1,49 @@
-// var Bcrypt = require('bcrypt')
-// var Boom = require('boom')
-//
-// var ../../utils/create-new-jwt = require('../../utils/create-new-jwt')
-// var ./pre/verify-unique-user = require('./pre/verify-unique-user')
-//
-//
+var User = require('../../model/User')
+
+var Bcrypt = require('bcrypt')
+var Boom = require('boom')
+var Joi = require('joi')
+
+
 module.exports = [
 	{
-		method: 'GET',
+		method: 'POST',
 		path: '/user',
 		config: {
+			description: 'Create new User',
 			tags: ['api'],
+	    validate: {
+	      payload: {
+	      	user: Joi.object().keys({
+					  email: Joi.string().email().required(),
+					  password: Joi.string().required(),
+	      	}).required()
+	      },
+	    },
+			pre: [{method: require('./pre/verify-unique-user')}]
 		},
 		handler: (request, reply) => {
-			reply('/GET user')
+    
+		  Bcrypt.genSalt(10, (err, salt) => {
+				
+				if (err) {
+					return reply(err)
+				}
+				
+		    Bcrypt.hash(request.payload.user.password, salt, (err, hashedPassword) => {
+    
+				  if (err) {
+	          return reply(err)
+	        }
+					
+					User({
+						email: request.payload.user.email,
+						password: hashedPassword
+					})
+					.save()
+					.then((user) => reply(user))
+					.catch((err) => reply(err))
+		    })
+		  })
 		}
-	}
-]
+	}]
