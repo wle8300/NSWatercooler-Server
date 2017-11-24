4bbf57c8951a2f8da87f11a889a66d3440bf553a
William Le <wle8300@gmail.com>
2017-11-24 11:09:58 -0800

    asdf

--- env.js
+++ env.js
@@ -12,7 +12,7 @@ module.exports = {
 	},
 	server: {
 		protocol: process.env.SERVER_PROTOCOL || 'http',
-		host: process.env.SERVER_HOST || '192.168.0.6',
+		host: process.env.SERVER_HOST || 'localhost',
 		port: process.env.SERVER_PORT || '3001'
 	},
 	dbg: {
